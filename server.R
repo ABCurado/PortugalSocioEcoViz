7bf55cff7788fe814cada0d9d20a788c433c646f
Antonio <antonio.estevao@xhock.com>
2019-01-11 16:17:53 +0000

    Data explorer now working correctly

--- server.R
+++ server.R
@@ -23,7 +23,7 @@ function(input, output, session) {
   
   output$scatterCollegeIncome <- renderPlot({
     # If no zipcodes are in view, don't plot
-    print(xyplot(Total_Average_income ~ Fraction_Superior, data = df_2015, xlim = range(df_2015$x), ylim = range(df_2015$y)))
+    print(xyplot(Total_Average_income ~ Fraction_Superior, data = df_2015))
   })
 
   # This observer is responsible for maintaining the circles and legend,
@@ -32,34 +32,21 @@ function(input, output, session) {
     colorBy <- input$color
     sizeBy <- input$size
 
-    if (colorBy == "superzip") {
-      # Color and palette are treated specially in the "superzip" case, because
-      # the values are categorical instead of continuous.
-      colorData <- ifelse(df_2015$centile >= (100 - input$threshold), "yes", "no")
-      pal <- colorFactor("viridis", colorData)
-    } else {
-      colorData <- df_2015[[colorBy]]
-      pal <- colorBin("viridis", colorData, 7, pretty = FALSE)
-    }
-    
-    if (sizeBy == "superzip") {
-      # Radius is treated specially in the "superzip" case.
-      radius <- ifelse(df_2015$centile >= (100 - input$threshold), 30000, 3000)
-    } else {
-      radius <- df_2015[[sizeBy]] / max(df_2015[[sizeBy]]) * 30000
-    }
+ 
+    pal <- colorFactor("viridis", df_2015$Winning_Party)
+    radius <- 30000
     
     leafletProxy("map", data = df_2015) %>%
       clearShapes() %>%
       addCircles(~x, ~y, radius=radius, layerId=~Municipality,
-                 stroke=FALSE, fillOpacity=0.4, fillColor=pal(colorData)) %>%
-      addLegend("bottomleft", pal=pal, values=colorData, title=colorBy,
+                 stroke=FALSE, fillOpacity=0.4, fillColor=pal(df_2015$Winning_Party)) %>%
+      addLegend("bottomleft", pal=pal, values=df_2015$Winning_Party, title=colorBy,
                 layerId="colorLegend")
   })
 
   # Show a popup at the given location
   showPopup <- function(municipality, lat, lng) {
-    selectedMunicipality <- df_2015[df_2015$municipality == municipality,]
+    selectedMunicipality <- df_2015[df_2015$Municipality == Municipality,]
     content <- as.character(tagList(
       tags$h4("Pop:", as.integer(selectedMunicipality$Total)),
       tags$strong(HTML(sprintf("%s %s",
@@ -68,7 +55,7 @@ function(input, output, session) {
       sprintf("Median household income: %s", dollar(selectedMunicipality$Total_Average_income)), 
       ags$br(),
     ))
-    leafletProxy("map") %>% addPopups(lng, lat, content, layerId = zipcode)
+    leafletProxy("map") %>% addPopups(lng, lat, content, layerId = Municipality)
   }
 
   # When map is clicked, show a popup with city info
@@ -87,44 +74,15 @@ function(input, output, session) {
   ## Data Explorer ###########################################
 
   observe({
-    cities <- if (is.null(input$states)) character(0) else {
-      filter(cleantable, State %in% input$states) %>%
+    Municipality <- if (is.null(input$Municipality)) character(0) else {
+      filter(cleantable, Municipality %in% input$Municipality) %>%
         `$`('City') %>%
         unique() %>%
         sort()
     }
-    stillSelected <- isolate(input$cities[input$cities %in% cities])
-    updateSelectInput(session, "cities", choices = cities,
+    stillSelected <- isolate(input$Municipality[input$Municipality %in% Municipality])
+    updateSelectInput(session, "Municipality", choices = Municipality,
       selected = stillSelected)
-  })
-
-  observe({
-    zipcodes <- if (is.null(input$states)) character(0) else {
-      cleantable %>%
-        filter(State %in% input$states,
-          is.null(input$cities) | City %in% input$cities) %>%
-        `$`('Zipcode') %>%
-        unique() %>%
-        sort()
-    }
-    stillSelected <- isolate(input$zipcodes[input$zipcodes %in% zipcodes])
-    updateSelectInput(session, "zipcodes", choices = zipcodes,
-      selected = stillSelected)
-  })
-
-  observe({
-    if (is.null(input$goto))
-      return()
-    isolate({
-      map <- leafletProxy("map")
-      map %>% clearPopups()
-      dist <- 0.5
-      zip <- input$goto$zip
-      lat <- input$goto$lat
-      lng <- input$goto$lng
-      showPopup(zip, lat, lng)
-      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
-    })
   })
 
   output$municipTable <- DT::renderDataTable({
@@ -132,7 +90,7 @@ function(input, output, session) {
       filter(
         Total >= input$minScore,
         Total <= input$maxScore,
-        is.null(input$Municipality) | Municipality %in% input$Municipality,
+        is.null(input$municipality) | Municipality %in% input$municipality,
       ) %>%
       mutate(Action = paste('<a class="go-map" href="" data-lat="', x, '" data-long="', y, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
     action <- DT::dataTableAjax(session, df)
