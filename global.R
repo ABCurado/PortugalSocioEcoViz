9c83ab93fee8da051f4fe91d297ef853aeaa5215
Antonio <antonio.estevao@xhock.com>
2019-01-11 13:48:10 +0000

    Added dataframes and how to import in global.R

--- global.R
+++ global.R
@@ -1,5 +1,18 @@
 library(dplyr)
 
+
+#df <- read.csv('data/dataframe_2015.csv', header = TRUE, sep = ',')
+#read.csv('data/dataframe_2015.csv', header = TRUE, sep = ',')
+#saveRDS(df, file="data/dataframe_2015.rds")
+
+#df <- read.csv('data/dataframe_2011.csv', header = TRUE, sep = ',')
+#read.csv('data/dataframe_2011.csv', header = TRUE, sep = ',')
+#saveRDS(df, file="data/dataframe_2011.rds")
+
+df_2011 <- readRDS("data/dataframe_2011.rds")
+df_2015 <- readRDS("data/dataframe_2015.rds")
+
+
 allzips <- readRDS("data/superzip.rds")
 allzips$latitude <- jitter(allzips$latitude)
 allzips$longitude <- jitter(allzips$longitude)
